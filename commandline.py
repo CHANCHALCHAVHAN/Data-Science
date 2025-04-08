f7e77e5c779641fb9b03c1f9dd9f02c8e5cdb48e
Chanchal Chavhan <143700585+CHANCHALCHAVHAN@users.noreply.github.com>
2025-04-08 12:52:47 +0530

    Create commandline.py

---
+++ commandline.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 20 09:26:19 2025
+
+@author: sakuc
+"""
+
+from sys import argv
+print("the first argument:",argv[1])
+print("Second argument: ",argv[2]) 
